6a28effca0b1fbd5a63824c673d4bdc4525c5cf3
Oscar Carbajal <oscar.carbajal.work@gmail.com>
2022-06-13 23:33:36 -0500

    conditonal (part 15 of assignment)

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -51,9 +51,19 @@ if(luckyNumber =='2'&& adventurous){
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
+
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if(pets <2){
+    console.log('I can have more pets');
+}
+
+else if(pets =2){
+    console.log('I have enough pets');
+}
+
 
 // STRETCH GOALS:
 
